05bea190750a39072c1826a6f8caedc404e5df3e
anran758 <anran758@gmail.com>
2020-05-29 20:32:27 +0800

    new: 新增 file-loader 来处理图片

---
+++ getting-started-loader-images/src/js/index.js
@@ -0,0 +1,39 @@
+import { log } from './log'
+
+import img1 from '../images/06.jpg';
+import img2 from '../images/webpack.png';
+import img3 from '../images/webpack.svg';
+
+import '../css/style.css';
+
+function installEvent() {
+  const panels = document.querySelectorAll('.panel')
+
+  function toggleOpen() {
+    panels.forEach(item => {
+      if (item === this) return;
+      item.classList.remove('open')
+    });
+
+    this.classList.toggle('open');
+  }
+
+  function toggleActicon(e) {
+    if (e.propertyName.includes('flex-grow')) {
+      this.classList.toggle('open-active')
+    }
+  }
+
+  // 给每个元素注册事件
+  panels.forEach(panel => {
+    panel.addEventListener('click', toggleOpen)
+    panel.addEventListener('transitionend', toggleActicon)
+  })
+}
+
+installEvent();
+log('测试图片引入~');
+
+console.log('img1 --> ', img1);
+console.log('img2 --> ', img2);
+console.log('img3 --> ', img3);
---
+++ getting-started-loader-images/webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  // 起点或是应用程序的起点入口
+  entry: "./src/js/index",
+
+  // 输出配置
+  output: {
+    // 编译后的输出路径
+    // 注意此处必须是绝对路径，不然 webpack 将会抛错（使用 Node.js 的 path 模块）
+    path: path.resolve(__dirname, "dist"),
+
+    // 输出 bundle 的名称
+    filename: "js/bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        // 匹配文件规则
+        test: /\.css$/i,
+        // use 从右至左进行应用
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../',
+            }
+          },
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|webp|svg)(\?.*)?$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: 'img/[name].[hash:8].[ext]'
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    // html-webpack-plugin
+    // https://github.com/jantimon/html-webpack-plugin#configuration
+    new HtmlWebpackPlugin({
+      title: 'Test Configuration',
+      template: path.resolve(__dirname, "./src/index.html"),
+    }),
+
+    // 提取 css 到单独的文件
+    // https://github.com/webpack-contrib/mini-css-extract-plugin
+    new MiniCssExtractPlugin({
+      // 选项类似于 webpackOptions.output 中的相同选项，该选项是可选的
+      filename: 'css/index.css',
+    })
+  ],
+}
